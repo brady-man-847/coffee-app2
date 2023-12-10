d6f8defeb2be6056d835392e286f89f32bdf6cee
brady <lji1016@midasit.com>
2023-12-10 18:47:01 +0900

    feat: 주문 시 orderlist 전달

--- src/components/room/detail/RoomDetailScreen.tsx
+++ src/components/room/detail/RoomDetailScreen.tsx
@@ -1,28 +1,47 @@
 import { useRouter } from 'next/router';
 import useQueryGetRoomInfo from '@/hooks/room/useQueryGetRoomInfo';
-import { Avatar, Button, Chip, Typography } from '@mui/material';
+import { Avatar, Button, Checkbox, Chip, Typography } from '@mui/material';
 import { stringAvatar } from '@/utils/muiAvatar';
 import { Loading } from '@/components/common';
 import RoomDetailInfo from '@/components/room/detail/RoomDetailInfo';
 import useMutationPayment from '@/hooks/payment/useMutationPayment';
+import { useState } from 'react';
 
 export default function RoomDetailScreen() {
+  const [checkedOrderList, setCheckedOrderList] = useState([0]);
   const router = useRouter();
   const { roomSn } = router.query;
 
-  const { data, isLoading } = useQueryGetRoomInfo({ req: { roomSn: Number(roomSn) } });
+  const { data, isLoading } = useQueryGetRoomInfo({
+    req: { roomSn: Number(roomSn) },
+    queryOption: {
+      onSuccess: (response) => {
+        setCheckedOrderList(response.orderList.map((i) => i.orderSn));
+      },
+    },
+  });
   const { mutate: payment } = useMutationPayment({});
 
   const handleClickPayment = () => {
+    if (checkedOrderList.length === 0) return window.alert('결제할 주문을 선택해주세요.');
+
     if (window.confirm(`방번호:${roomSn} 결제하시겠습니까?`)) {
       payment(
-        { roomSn: Number(roomSn) },
+        { roomSn: Number(roomSn), orderSnList: checkedOrderList },
         {
           onSuccess: (result) => {
             window.alert(`결제가 완료되었습니다.\n 주문번호: ${result.orderNo}`);
           },
         },
       );
+    }
+  };
+
+  const handleChangeOrderItemCheck = (orderSn: number, checked: boolean) => {
+    if (checked) {
+      setCheckedOrderList((prev) => [...prev, orderSn]);
+    } else {
+      setCheckedOrderList((prev) => prev.filter((i) => i !== orderSn));
     }
   };
 
@@ -50,12 +69,19 @@ export default function RoomDetailScreen() {
 
           <div className={'order-wrapper'}>
             {data.orderList.map((order) => {
-              const { menu, optionList, memberNickname, memberSn } = order;
+              const { menu, optionList, memberNickname, memberSn, quantity } = order;
               const { name: menuName, optionGroupList } = menu;
               const includeOptionList = optionGroupList.flatMap((i) => i.optionList).filter((j) => optionList.includes(j.code));
               return (
                 <div className={'order-item'} key={`${order.memberSn}_order_item`}>
-                  <Avatar {...stringAvatar(memberNickname)} />
+                  <div className={'avatar-item-wrapper'}>
+                    <Checkbox
+                      defaultChecked
+                      value={order.orderSn}
+                      onChange={(_e, checked) => handleChangeOrderItemCheck(order.orderSn, checked)}
+                    />
+                    <Avatar {...stringAvatar(memberNickname)} />
+                  </div>
                   <div>
                     <Chip
                       sx={{
@@ -80,6 +106,7 @@ export default function RoomDetailScreen() {
                         label={i.name}
                       />
                     ))}
+                    <span> X {quantity}</span>
                   </div>
                 </div>
               );
@@ -125,7 +152,13 @@ export default function RoomDetailScreen() {
             padding: 8px;
             border: 1px solid lightgray;
           }
-
+          .avatar-item-wrapper {
+            display: flex;
+            flex-direction: row;
+            justify-content: center;
+            align-items: center;
+            gap: 4px;
+          }
           .payment-wrapper {
           }
         `}</style>
