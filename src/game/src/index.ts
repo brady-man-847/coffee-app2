f61ebb3381adccfc4bfd5336658f6c2bb6717de0
brady <lji1016@midasit.com>
2023-12-20 11:05:20 +0900

    feat: remove game

--- src/game/src/index.ts
+++
@@ -1,6 +0,0 @@
-import {Roulette} from './roulette';
-import options from './options';
-
-const roulette = new Roulette();
-(window as any).roullete = roulette;
-(window as any).options = options;
--- src/game/src/options.ts
+++
@@ -1,7 +0,0 @@
-class Options {
-    useSkills: boolean = true;
-    winningRank: number = 0;
-}
-
-const options = new Options();
-export default options;
